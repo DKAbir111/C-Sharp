b9d4bfa65f1e0b5162ca5e3144fe8f2866921fdc
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-13 23:04:55 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = new int[5];
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("Enter a number:");
+                name[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
