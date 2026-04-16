7af5362b3871fad49f17abb464c2c100ec16192d
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-16 23:45:27 +0600

    gitignore added

---
+++ OOP.cs
@@ -0,0 +1,8 @@
+// OOP.cs
+public class OOP
+{
+    public void Run()
+    {
+        Console.WriteLine("OOP file running");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
+// var builder = WebApplication.CreateBuilder(args);
+// // Swagger services
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// var app = builder.Build();
 
-var app = builder.Build();
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-app.MapGet("/", () =>
+// app.MapGet("/", () =>
+// {
+//     return "Hello Server is running successfully...";
+// });
+
+// app.MapGet("/hello", () =>
+// {
+//     var response = new
+//     {
+//         message = "Successfully called the /hello endpoint",
+//         success = true
+//     };
+//     return response;
+// });
+
+
+// var products = new List<Product>
+// {
+//     new Product(1, "Laptop", 999.99m),
+//     new Product(2, "Smartphone", 499.99m),
+//     new Product(3, "Headphones", 199.99m)
+
+// };
+
+// app.MapGet("/products", () =>
+// {
+//     return Results.Ok(products);
+// });
+
+// app.Run();
+// var oop = new OOP();
+// oop.Run();
+
+// public record Product(int Id, string Name, decimal Price);
+
+
+class Person
 {
-    return "Server is running...";
-});
+    public string name;
+    public int age;
 
-app.MapGet("/hello", () =>
+}
+class Program
 {
-    return "Hello World!";
-});
-
-app.Run();
-
+    static void Main(string[] args)
+    {
+        var person = new Person();
+        person.name = "John Corner";
+        person.age = 30;
+        Console.WriteLine($"Name:{person.name}, Age: {person.age}");
+    }
+}
