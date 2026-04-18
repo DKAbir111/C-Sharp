b54cc19ce2d3a71994bd6cadaaeb40f804461a29
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-18 23:38:36 +0600

    practicing problem

--- Program.cs
+++ Program.cs
@@ -3,17 +3,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number = 0;
 
-        Console.WriteLine("Give a number:");
-        number = int.Parse(Console.ReadLine());
-        if (number % 2 == 0)
-        {
-            Console.WriteLine($"{number} is even");
-        }
-        else
-        {
-            Console.WriteLine($"{number} is odd");
-        }
+        // Even Odd
+
+        // int number = 0;
+        // Console.WriteLine("Give a number:");
+        // number = int.Parse(Console.ReadLine());
+        // if (number % 2 == 0)
+        // {
+        //     Console.WriteLine($"{number} is even");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{number} is odd");
+        // }
+
+
+        //Array sum
+
+        // int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        // int sum = 0;
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     sum = sum + numbers[i];
+        // }
+        // Console.WriteLine($"The sum of the numbers is: {sum}");
+
+
+        // Max Number
+
+
+        // int num1, num2, num3;
+        // Console.WriteLine("Give three numbers:");
+        // num1 = int.Parse(Console.ReadLine());
+        // num2 = int.Parse(Console.ReadLine());
+        // num3 = int.Parse(Console.ReadLine());
+
+        // if (num1 > num2 && num1 > num3)
+        // {
+        //     Console.WriteLine($"{num1} is the largest number");
+        // }
+        // else if (num2 > num1 && num2 > num3)
+        // {
+        //     Console.WriteLine($"{num2} is the largest number");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{num3} is the largest number");
+        // }
     }
 }
